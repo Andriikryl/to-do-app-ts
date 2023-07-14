5a6b474f647f139863561904c0d3af82dba0daeb
Andriikryl <andreyk557@gmail.com>
2023-07-14 15:07:12 +0300

    add toast notifications

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
 import { toast } from "react-hot-toast";
-import { useTodo } from "../context";
+import { useTodo } from "../context/useTodo";
 import { Input } from "./Input";
 
 export const AddTodo = () => {
   const [input, setInput] = useState<string>("");
-  const [todos, setTodos] = useState<string[]>([]);
   const inputRef = useRef<HTMLInputElement>(null);
+  const { addTodo } = useTodo();
 
   useEffect(() => {
     if (inputRef.current) {
@@ -14,33 +14,34 @@ export const AddTodo = () => {
     }
   }, []);
 
-  const handelSubmission = (e: React.FormEvent) => {
+  const handleSubmission = (e: React.FormEvent) => {
     e.preventDefault();
     if (input.trim() !== "") {
-      setTodos([...todos, input]);
+      addTodo(input);
       setInput("");
+      toast.success("Todo added successfully!");
+    } else {
+      toast.error("Todo field cannot be empty!");
     }
   };
+
   return (
-    <div>
-      <form onSubmit={handelSubmission}>
-        <div className="flex items-center w-full max-w-lg gap-2 p-5 m-auto">
-          <Input
-            ref={inputRef}
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            type="text"
-            className="w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zinc-500 focus:border-white"
-            placeholder="start typing ..."
-          />
-          <button
-            type="submit"
-            className="px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-xl"
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
+    <form onSubmit={handleSubmission}>
+      <div className="flex items-center w-full max-w-lg gap-2 p-5 m-auto">
+        <Input
+          ref={inputRef}
+          type="text"
+          placeholder="start typing ..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-xl"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
   );
 };
