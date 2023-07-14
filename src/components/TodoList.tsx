69b3b3085ea2f6538e240a60873e97786cb73902
Andriikryl <andreyk557@gmail.com>
2023-07-14 15:15:09 +0300

    add todoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,24 @@
-import { TodoItem } from "./TodoItem";
-import { useTodo } from "../context";
+import { useTodo } from "../context/useTodo";
 import { SiStarship } from "react-icons/si";
 
 export const TodoList = () => {
-  return <div>TodoList</div>;
+  const { todos } = useTodo();
+
+  if (!todos.length) {
+    return (
+      <div className="max-w-lg px-5 m-auto">
+        <h1 className="flex flex-col items-center gap-5 px-5 py-10 text-xl font-bold text-center rounded-xl bg-orange-200 text-yellow-400">
+          You have nothing to do!
+        </h1>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="grid max-w-lg gap-2 px-5 m-auto">
+      {todos.map((todo) => (
+        <li key={todo}>{todo}</li>
+      ))}
+    </ul>
+  );
 };
